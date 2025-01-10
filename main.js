d6652ef1857d2368ad11d38e98e9fb6103f310b8
westerntoad <abr.engebretson@gmail.com>
2025-01-10 11:38:05 -0800

    rotating pookas & algorithm0r

--- main.js
+++ main.js
@@ -5,11 +5,16 @@ const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./sheet.png");
 
 ASSET_MANAGER.downloadAll(() => {
-  const canvas = document.getElementById("gameWorld");
-  const ctx = canvas.getContext("2d");
+    const canvas = document.getElementById("gameWorld");
+    const ctx = canvas.getContext("2d");
 
-  gameEngine.init(ctx);
+    gameEngine.init(ctx);
 
-  gameEngine.start();
-  gameEngine.addEntity(new Pooka(gameEngine, 10, 10));
+    gameEngine.start();
+
+    const numPookas = 3
+    const pookas = []
+    for (let i = 0; i < numPookas; i++) {
+        gameEngine.addEntity(new Pooka(gameEngine, canvas.width / 2, canvas.height / 2, ((2*Math.PI) / numPookas) * i));
+    }
 });
--- pooka.js
+++ pooka.js
@@ -1,18 +1,28 @@
 class Pooka {
-    constructor(game, x, y) {
-        Object.assign(this, { game, x, y });
+    constructor(game, origX, origY, polar, speed) {
+        Object.assign(this, { game, origX, origY, polar });
         this.spritesheet = ASSET_MANAGER.getAsset("./sheet.png");
         
     };
 
-    update() { /* ~ unused ~ */ }
+    update() {
+        const speed = 0.02
+
+        this.polar += speed
+    }
 
     draw(ctx) {
-        let frame = Math.floor(((Date.now() / 1000) % 1) / 0.25);
-        let sX = 1 + (frame % 2) * 33;
-        let sY = 147 + (Math.floor(frame / 2) % 2) * 16;
+        const radius = 300;
+        const scale = 5;
+        const w = 13;
+        const h = 12;
+        const frame = Math.floor(((Date.now() / 1000) % 1) / 0.25);
+        const sX = 1 + (frame % 2) * 33;
+        const sY = 147 + (Math.floor(frame / 2) % 2) * 16;
+        const dX = this.origX + Math.cos(this.polar) * radius - (w / 2);
+        const dY = this.origY + Math.sin(this.polar) * radius - (h / 2);
         
-        ctx.drawImage(this.spritesheet, sX, sY, 13, 12, this.x, this.y, 130, 120);
+        ctx.drawImage(this.spritesheet, sX, sY, w, h, dX, dY, w * scale, h * scale);
     };
 };
 
